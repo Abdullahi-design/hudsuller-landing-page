5777c5f3cc50cea8c82e9238d95dcc07b8a05376
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-03-28 02:31:53 +0100

    added takeaway block in hero section

---
+++ components/block/TakeAways.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+import { BsFillPatchCheckFill } from 'react-icons/bs';
+
+const TakeAways = () => {
+    const takeAways = [
+        {title: 'Learn from real life experts', icon: <BsFillPatchCheckFill className='w-5 h-5'/> },
+        {title: 'Become Something you love', icon: <BsFillPatchCheckFill className='w-5 h-5'/> },
+        {title: 'Market, Sell, And Deliver Like A Pro', icon: <BsFillPatchCheckFill className='w-5 h-5'/> },
+    ]
+  return (
+    <div className='md:flex hidden justify-center gap-8'>
+        {
+            takeAways.map(takeAway => (
+                <div className='flex gap-3'>
+                    <p className='text-button-primary'>{takeAway.icon}</p>
+                    <p>{takeAway.title}</p>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default TakeAways
