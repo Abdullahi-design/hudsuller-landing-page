43cb66d6dd9c1b551e6fde3680bcf5e7650d39f0
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-03-31 07:02:17 +0100

    link to academy importation calsses

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -9,7 +9,9 @@ const HomeHero = () => {
       {/* <h2 className='sm:text-4xl text-2xl capitalize py-2'>Our Expert Instructors Will Teach You How!</h2> */}
       <p className='sm:text-2xl text-xl font-thin mt-3'>Our team of experts are people who are really good at what they love to do. They will teach you how to focus on one thing and get really good at it too. With our online courses, you will learn from the best and get the skills you need to be great at what you want to do.</p>
       <div className='md:py-16 py-8'>
-        <button className='cta-button'>Join a master class Now {`>>`}</button>
+        <Link href={'/academy'}>
+          <button className='cta-button'>Join a master class Now {`>>`}</button>
+        </Link>
         <TakeAways/>
       </div>
     </div>
